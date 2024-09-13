d877565043da32c91c1ee1678f5d4da1eef83240
Craig Edmunds <craig.edmunds@gmail.com>
2024-09-13 16:43:38 +0100

    Adds simple notifications UI (#1)

--- src/server/notifications/controller.js
+++ src/server/notifications/controller.js
@@ -1,10 +1,55 @@
+import { config } from '~/src/config/index.js'
+import { map } from 'lodash-es'
 /**
  * A GDS styled example notifications page controller.
  * Provided as an example, remove or modify as required.
  * @satisfies {Partial<ServerRoute>}
  */
+// import { proxyFetch } from '~/src/server/common/helpers/proxy-fetch'
+import { createLogger } from '~/src/server/common/helpers/logging/logger.js'
+
+import JsonApi from 'devour-client'
+
+const jsonApi = new JsonApi({ apiUrl: config.get('tdmBackendApi') })
+
+// Define Model
+jsonApi.define('ipaffsNotification', {
+  version: '',
+  notificationStatus: '',
+  notificationType: '',
+  lastUpdated: '',
+  partOne: {
+    commodities: {
+      numberOfPackages: 0
+    }
+  }
+})
+
 export const notificationsController = {
-  handler(request, h) {
+  async handler(request, h) {
+    const logger = createLogger()
+
+    const { data } = await jsonApi.findAll('ipaffsNotifications')
+    logger.info(data[0])
+    var notifications = map(data, (n) => [
+      {
+        text: n.id
+      },
+      {
+        text: n.notificationType
+      },
+      {
+        text: n.lastUpdated
+      },
+      {
+        text: n.partOne.commodities.numberOfPackages,
+        format: 'numeric',
+        attributes: {
+          'data-sort-value': '1897'
+        }
+      }
+    ])
+
     return h.view('notifications/index', {
       pageTitle: 'Notifications',
       heading: 'Notifications',
@@ -16,7 +61,8 @@ export const notificationsController = {
         {
           text: 'Notifications'
         }
-      ]
+      ],
+      notifications
     })
   }
 }
