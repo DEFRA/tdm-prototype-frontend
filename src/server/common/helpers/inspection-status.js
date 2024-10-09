1b088d2d9f1f05de9e5f819c531148e384e736fa
Craig Edmunds <craig.edmunds@gmail.com>
2024-10-09 14:40:16 +0100

    Fixes inspection bug

--- src/server/common/helpers/inspection-status.js
+++ src/server/common/helpers/inspection-status.js
@@ -2,11 +2,13 @@ import { createLogger } from '~/src/server/common/helpers/logging/logger.js'
 const logger = createLogger()
 function inspectionStatusElementListItem(commodity) {
   logger.debug(`inspectionStatusElementListItem : ${JSON.stringify(commodity)}`)
-  const inspectionRequired = commodity
+  const inspectionRequired = commodity.riskAssesment
+    ? commodity.riskAssesment.riskDecision
+    : false
 
   return {
     kind: 'tag',
-    value: commodity.riskAssesment.riskDecision,
+    value: inspectionRequired ? 'Yes' : 'No',
     classes: inspectionRequired ? 'govuk-tag--red' : 'govuk-tag--green'
   }
 }
